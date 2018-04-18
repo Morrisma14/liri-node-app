fd21ff2e668660f2ec5e803e94fe6f04c53157e3
Madison Morris <madsmorris14@gmail.com>
2018-04-18 12:44:41 -0400

    checking if up-to-date

--- javascript/liri.js
+++ javascript/liri.js
@@ -1,17 +1,19 @@
 require("dotenv").config();
 
-var client = new Twitter(keys.twitter);
 
 const dotenv = require('dotenv').config();
 const keys = require('./keys');
 const request = require('request');
 const Twitter = require('twitter');
-const Spotify = require('spotify');
+const Spotify = require('node-spotify-api');
 const fs = require('file-system');
 
-const client = new Twitter(keys.twitter);
+var spotify = new Spotify({
+  id: keys.spotify.id,
+  secret: keys.spotify.secret
+});
 
-var arg = process.argv[2];
+const client = new Twitter(keys.twitter);
 
 const params = {
 	screen_name: 'madsmorris14',
@@ -30,57 +32,59 @@ var tweets = function(){
 });
 };
 
-if (arg === 'my-tweets') {
-	tweets();
-};
-
-
 // Spotify
-var q = process.argv[3];
-if (q === undefined) {
-  q = 'I want it that way'
-};
 
-
-var songs = function() {spotify.search({type: 'track', query: q, limit: 1}, function(err, data)
-  {if(q !== null){
-    console.log(data.tracks.items[0].album.artists[0].name); 
-    console.log(data.tracks.items[0].name);
-    console.log(data.tracks.items[0].album.name);
-    console.log(data.tracks.items[0].external_urls.spotify);
+var songs = function() {
+  var q = process.argv[3];
+    if (q === undefined) {
+      q = 'I want it that way'
+    };
+  spotify.search({type: 'track', query: q, limit: 1}, function(err, data) {
+    if(err) {throw err;}    
+    console.log(data);
+    if(q !== null){
+      console.log(data.tracks.items[0].album.artists[0].name); 
+      console.log(data.tracks.items[0].name);
+      console.log(data.tracks.items[0].album.name);
+      console.log(data.tracks.items[0].external_urls.spotify);
     }
   });
-};
-
-if (arg === 'spotify-this-song') {
-  songs();
 };
 
 
 //OMDB
 
-// OMBD portion
-var movie = process.argv[3];
-
 
 function movieRequest () {
-  request("http://www.omdbapi.com/?i=tt3896198&apikey=94903d55" + movie, function(error, response, body) {
-
-  // If there were no errors and the response code was 200 (i.e. the request was successful)...
+  var movie = process.argv[3];
+  var url = "http://www.omdbapi.com/?apikey=" + keys.omdb.apiKey + "&t=" + movie;
+  console.log("url: ", url)
+  request(url, function(error, response, body) {
+    var resBody = JSON.parse(body);
   if (!error) {
-    // Then we print out the imdbRating
-    console.log("Title: " + JSON.parse(body).Title);
-    console.log("Year Released: " + JSON.parse(body).Released);
-    console.log("imdbRating: " + JSON.parse(body).imdbRating);
-    console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
-    console.log("Country: " + JSON.parse(body).Country);
-    console.log("Language: " + JSON.parse(body).Language);
-    console.log("Plot: " + JSON.parse(body).Plot);
-    console.log("Cast: " + JSON.parse(body).Actors);
+    console.log("Title: " + resBody.Title);
+    console.log("Year Released: " + resBody.Released);
+    console.log("imdbRating: " + resBody.imdbRating);
+    console.log("Rotten Tomatoes Rating: " + resBody.Ratings[1].Value);
+    console.log("Country: " + resBody.Country);
+    console.log("Language: " + resBody.Language);
+    console.log("Plot: " + resBody.Plot);
+    console.log("Cast: " + resBody.Actors);
   }
 });
 }
 
-if (arg === 'movie-this') {
-  movieRequest();
+function init() {
+  var arg = process.argv[2];
+  if (arg === 'movie-this') {
+    movieRequest();
+  }
+  else if (arg === 'spotify-this-song') {
+    songs();
+  }
+  else if (arg === 'my-tweets') {
+    tweets();
+  };  
 }
+
+init();
